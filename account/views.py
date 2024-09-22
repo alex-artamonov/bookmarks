f9e3f59d0fbd627faec04bfad3aadac9e68df565
alex-artamonov <alex.artamonov2010@gmail.com>
2024-09-22 23:23:31 +0200

    finish step user tracking actions

--- account/views.py
+++ account/views.py
@@ -8,14 +8,22 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
-from .models import Profile
-from .models import Contact
-
+from .models import Profile, Contact
+from actions.utils import create_action
+from actions.models import Action
 
 @login_required
 def dashboard(request):
-    return render(request, "account/dashboard.html", {"section": "dashboard"})
-
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+    if following_ids:
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions.select_related('user', 'user__profile')\
+        .prefetch_related('target')[:10]
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard',
+                   'actions': actions})
 
 def user_login(request):
     if request.method == "POST":
@@ -46,6 +54,7 @@ def register(request):
             new_user.set_password(user_form.cleaned_data["password"])
             new_user.save()
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
         return render(request, "account/register_done.html", {"new_user": new_user})
     else:
         user_form = UserRegistrationForm
@@ -115,6 +124,7 @@ def user_follow(request):
                     user_from=request.user,
                     user_to=user
                 )
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user,
                                        user_to=user).delete()
@@ -122,3 +132,4 @@ def user_follow(request):
         except User.DoesNotExist:
             return JsonResponse({'status':'error'})
     return JsonResponse({'status':'error'})
+
